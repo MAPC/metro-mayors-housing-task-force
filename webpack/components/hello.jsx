b6ed233987d9043719f26a894213a3d514077908
Matthew Zagaja <mzagaja@alum.wpi.edu>
2018-05-08 16:54:05 -0400

    Spike Layout + React Integration
    * Integrate Webpacker
    * Spike slide show
    * Integrate React into Jekyll

---
+++ webpack/components/hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>Hey, y’all!!</div>
+    );
+  }
+}
+
+export default Hello;
---
+++ webpack/entry.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './components/hello.jsx';
+
+function App() {
+  return (
+    <Hello />
+  );
+}
+
+render(<App />, document.getElementById('root'));
