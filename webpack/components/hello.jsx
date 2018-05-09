952ebe305981dbf426c2ff6d4c715b803b8ea448
Matthew Zagaja <mzagaja@alum.wpi.edu>
2018-05-09 16:03:10 -0400

    Spike Topic Buttons

--- webpack/components/hello.jsx
+++ webpack/components/hello.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 
 class Hello extends Component {
   render() {
--- webpack/entry.jsx
+++ webpack/entry.jsx
@@ -1,11 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './components/hello.jsx';
+import topicData from './topic-data.json';
 
 function App() {
   return (
-    <Hello />
+    <Topics />
   );
 }
 
-render(<App />, document.getElementById('root'));
+class Topics extends React.Component {
+  render() {
+    return (
+      <div className="topic-buttons">
+        {
+          topicData.map(function(topic) {
+              const subTopicButtons = topic.subTopics.map(function(subTopic) {
+                return (
+                  <button className='sub-topic'>{subTopic.title}</button>
+                );
+              })
+              return (
+                [<button className='topic'>{topic.title}</button>, subTopicButtons]
+              );
+          })
+        }
+      </div>
+    );
+  }
+}
+
+// this.props.data.participants.map(function(player) {
+//    return <li key={player.championId}>{player.summonerName}</li>
+
+// function TopicButton() {
+//   return (
+//     <button className="topic-button">Topic Name</button>
+//   );
+// }
+
+// function BestPractice() {
+//   return (
+//     <div>
+//       <div className="best-practice">
+//         <h2>This is a best practice</h2>
+//         <p>This is the text of the best practice</p>
+//       </div>
+
+//       <div className="best-practice-image">
+//         <img alt="an interesting thing" src="" />
+//       </div>
+//     </div>
+//   );
+// }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
