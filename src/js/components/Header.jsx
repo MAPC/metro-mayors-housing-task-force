f6dc8a09b3831dfca1d560120f900fc4dd67c12b
Eric Youngberg <eyoungberg@mapc.org>
2018-05-28 09:29:16 -0400

    Link best-practices page in navbar

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -16,7 +16,7 @@ class Header extends React.Component {
         <nav>
           <ul>
             <li><Link to={'/about'}>About</Link></li>
-            <li><Link to={'/about'}>Best Practices</Link></li>
+            <li><Link to={'/best-practices'}>Best Practices</Link></li>
             <li><Link to={'/about'}>Story</Link></li>
           </ul>
         </nav>
