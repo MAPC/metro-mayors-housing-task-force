d161f9f985ed6d6b18428978158ab986ec322662
Matthew Zagaja <mzagaja@alum.wpi.edu>
2018-06-12 14:46:50 -0400

    Put municipality text inside bubble
    This commit puts the municipality name inside the bubble when in mobile view.

--- src/js/components/home/Member.jsx
+++ src/js/components/home/Member.jsx
@@ -9,14 +9,16 @@ class Member extends React.Component {
         className="component Member"
         to={this.props.municipalityName.toLowerCase()}
       >
-        <img
-          alt={`${this.props.title} ${this.props.name} from ${this.props.municipalityName}`}
-          src={`/assets/images/${this.props.municipalityName}-small.png`}
-        />
+        <div className="image-container" style={{marginRight: '1em'}}>
+          <img
+            alt={`${this.props.title} ${this.props.name} from ${this.props.municipalityName}`}
+            src={`/assets/images/${this.props.municipalityName}-small.png`}
+          />
+          <div className="municipality-name">{this.props.municipalityName}</div>
+        </div>
         <div className="name-title">
           <div>{this.props.title}</div>
           <div className="name">{this.props.name}</div>
-          <div>{this.props.municipalityName}</div>
         </div>
       </Link>
     );
