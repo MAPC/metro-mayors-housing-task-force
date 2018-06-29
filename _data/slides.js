3167c219d2cbd6f20eb79a83ba813097d8d8ab23
Eric Youngberg <eyoungberg@mapc.org>
2018-06-29 09:32:23 -0400

    Add available splash images to slides

--- _data/slides.js
+++ _data/slides.js
@@ -27,20 +27,30 @@ module.exports = [
     content: "Some are being priced out of their neighborhoods.",
   },
   {
-    image: 'birds-eye-1.jpg',
+    image: 'commuter-rail.jpg',
     content: "This is hardest for low-income residents who face fewer housing options and longer commutes to get to their jobs.",
   },
   {
-    image: 'birds-eye-1.jpg',
+    image: 'island-1.jpg',
     content: "The high cost of housing impacts our region's economy: workers are moving to other cities to find housing they can afford.",
+    credit: {
+      artist: 'Osman Rana',
+    },
   },
   {
-    image: 'birds-eye-1.jpg',
+    chart: '',
     content: "Our region is growing fast: In the last the last decade, 45% of Massachusetts job growth has been concentrated in Metro Boston's inner 15 communities.",
   },
   {
-    image: 'birds-eye-1.jpg',
+    chart: '',
     content: "With that growth, our region is gaining jobs and workers faster than we are building new homes.",
+  },
+  {
+    image: 'skyline-1.jpg',
+    content: "The Innovation Economy is attracting highly educated workers with high incomes, who can pay more for housing than others.",
+    credit: {
+      artist: 'Qusai Akoud',
+    },
   },
   {
     content: "This drives up costs for everyone.",
@@ -50,19 +60,21 @@ module.exports = [
     content: "Baby boomers are retiring (800k a year in Massachusetts). Job vacancies are opening to younger generations.",
   },
   {
-    image: 'birds-eye-1.jpg',
+    image: 'hedge-garden.jpg',
     content: "Many Seniors are hoping to stay in their communities after retirement, but can't afford to downsize. They remain at home which limits housing open to new workers.",
+    credit: {
+      artist: 'Joyce Huis',
+    },
   },
   {
     image: 'birds-eye-1.jpg',
     content: "Speculative investment and the conversion of residential homes to hotel-like home sharing takes units off the rental market, depleting our already low supply.",
   },
   {
-    image: 'birds-eye-1.jpg',
     content: "So why don't we just build more homes?",
   },
   {
-    image: 'birds-eye-1.jpg',
+    chart: '',
     content: "The cost of building is higher than ever.",
   },
   {
@@ -70,7 +82,6 @@ module.exports = [
     content: "Some local laws overly restrict the type and amount of housing allowed.",
   },
   {
-    image: 'birds-eye-1.jpg',
     content: "Time consuming permitting processes and inconsistent approval processes are too risky to some real estate developers",
   },
   {
@@ -78,7 +89,7 @@ module.exports = [
     content: "Community concerns about too much density or lack of affordable housing can stall development completely.",
   },
   {
-    image: 'birds-eye-1.jpg',
+    chart: '',
     content: "But we know housing demand will only continue to grow. More workers will need homes.",
   },
   {
@@ -90,7 +101,6 @@ module.exports = [
     content: "Housing demand will be felt across our region. This isn't one type of housing, or in a single community.",
   },
   {
-    image: 'birds-eye-1.jpg',
     content: "No one community can create a solution alone.",
   },
   {
@@ -98,19 +108,24 @@ module.exports = [
     content: "All our cities and towns must work together to meet the needs of today and anticipate the needs of tomorrow.",
   },
   {
-    image: 'birds-eye-1.jpg',
+    image: 'mayors.jpg',
     content: "The 15 leaders of the Metro Mayors Coalition have pledged to build 180k homes by 2030.",
   },
   {
-    image: 'birds-eye-1.jpg',
     content: "This will require strong partnership between the business community and public sector.",
   },
   {
-    image: 'birds-eye-1.jpg',
+    image: 'river-houses.jpg',
     content: `${wrap('b', 'We will need')}${br}good planning${br}good governance${br}more affordable units${br}and protection for existing tenants.`,
+    credit: {
+      artist: 'Qusai Akoud',
+    },
   },
   {
-    image: 'birds-eye-1.jpg',
+    image: 'skyline-2.jpg',
     content: wrap('b', "Let's get started today"),
+    credit: {
+      artist: 'Qusai Akoud',
+    },
   },
 ];
