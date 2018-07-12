1e9257d0994e2b8cfeeed38a83551a927cd6eec6
Ian Hill <ihill@mapc.org>
2018-07-12 08:36:39 -0400

    Change headline

--- src/js/components/home/IntroSection.jsx
+++ src/js/components/home/IntroSection.jsx
@@ -6,7 +6,7 @@ class IntroSection extends React.Component {
   render() {
     return (
       <section className="component IntroSection container">
-        <h1 className="huge">140,000 new homes in Metro Boston</h1>
+        <h1 className="huge">Housing Metro Boston</h1>
         <p>
           Meet the 15 leaders united in a landmark regional commitment to housing production.
         </p>
