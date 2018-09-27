e12d9e6752188a1e0db0ed318058ac07e3bf9c9b
Eric Youngberg <eyoungberg@mapc.org>
2018-09-27 08:58:49 -0400

    Add links to DataCommon for Member components

--- src/js/components/home/Member.jsx
+++ src/js/components/home/Member.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
-import colors from '~/_data/colors';
-
+const normalize = x => x.split(' ').join('-').toLowerCase();
 
 class Member extends React.Component {
 
   render() {
     return (
       <div className="component Member">
-        <div className={`image-container ${this.props.color}`}>
-          <h4>{this.props.municipalityName}</h4>
-        </div>
+        <a href={`https://datacommon.mapc.org/profile/${normalize(this.props.municipalityName)}/housing`}>
+          <div className={`image-container ${this.props.color}`}>
+            <h4>{this.props.municipalityName}</h4>
+          </div>
+        </a>
 
         <div className="name">{this.props.name}</div>
         <div className="title">{this.props.title}</div>
