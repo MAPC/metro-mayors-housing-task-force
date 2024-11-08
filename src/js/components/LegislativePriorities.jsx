ff6d8981c6fdf0993c93ffbf2ab832267272b019
Jonathan McKenzie <jmckenzie@mapc.org>
2024-11-08 15:24:43 -0500

    Improve table rendering for legislative priorities, use Figure component for Tracking Progress images and add temporary hack for image sizing, use new version of airtable-cms that fixes name/unique id bug

--- src/js/components/LegislativePriorities.jsx
+++ src/js/components/LegislativePriorities.jsx
@@ -1,13 +1,23 @@
+import { useCallback } from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import Table from "react-bootstrap/Table";
 
 import CMSComponent from "./CMSComponent";
 
 const LegislativePriority = ({ name, content }) => {
+  const PartialTable = useCallback((props) => <Table bordered hover {...props} />, []);
   return (
     <div key={name} className="container">
       <h2>{name}</h2>
-      <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
+      <Markdown
+        components={{
+          table: PartialTable,
+        }}
+        remarkPlugins={[remarkGfm]}
+      >
+        {content}
+      </Markdown>
     </div>
   );
 };
