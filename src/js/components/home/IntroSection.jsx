7e4019eb86bcf0af887a03a3525392c6df6a278c
Jonathan McKenzie <jmckenzie@mapc.org>
2024-11-14 12:06:20 -0500

    Bumping member community count to 17 - hardcoded for now, but we can pull this from the CMS in the future

--- src/js/components/home/IntroSection.jsx
+++ src/js/components/home/IntroSection.jsx
@@ -2,7 +2,7 @@ const IntroSection = () => {
   return (
     <section className="component IntroSection container">
       <h1 className="huge">Housing Metro Boston</h1>
-      <p>15 cities and towns united in a landmark regional commitment to housing production.</p>
+      <p>17 cities and towns united in a landmark regional commitment to housing production.</p>
 
       <div className="header-image">
         <img src="/assets/images/header-homes.jpg" alt="Multi-family homes" />
